111535a5209bc8fd563cc7deaa52809b6745ce88
RafaelJM <31122807+RafaelJM@users.noreply.github.com>
2019-11-24 16:53:58 -0300

    Add files via upload

--- Trabalho.py
+++ Trabalho.py
@@ -4,21 +4,21 @@
 """
 
 import cv2 as cv
+import numpy as np
+from scipy import interp
+import matplotlib.pyplot as plt
+from itertools import cycle
+
 from sklearn.datasets import fetch_lfw_people
 from sklearn.model_selection import train_test_split
 from sklearn.decomposition import PCA as sklearn_PCA
 from sklearn.multiclass import OneVsRestClassifier
-from sklearn.metrics import roc_curve, auc, f1_score, confusion_matrix
+from sklearn.metrics import roc_curve, auc, f1_score, confusion_matrix, precision_recall_curve, average_precision_score
 from sklearn.neural_network import MLPClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.preprocessing import label_binarize
 from skimage.feature import local_binary_pattern
 from sklearn.svm import SVC
-
-import numpy as np
-from scipy import interp
-import matplotlib.pyplot as plt
-from itertools import cycle
 
 #--- tools
 def writeDataBase(Data,Target,imageShape,min_faces_per_person):
@@ -63,22 +63,22 @@ def LBP(Data_train, Data_test, imageShape, numPoints = 27, radius = 3):
 
 def PCA_ML(Data_train, Data_test, Target_train, Target_test):
     Data_train, Data_test = PCA(Data_train, Data_test)
-    ml = OneVsRestClassifier(MLPClassifier(max_iter = 1000)).fit(Data_train, Target_train)
-    return (ml.predict_proba(Data_test), ml.predict(Data_test))
+    ml = OneVsRestClassifier(MLPClassifier(max_iter = 2000)).fit(Data_train, Target_train)
+    return {"predict_proba": ml.predict_proba(Data_test),"predict": ml.predict(Data_test)}
 
 def LBP_ML(Data_train, Data_test, Target_train, Target_test, imageShape):
     Data_train, Data_test = LBP(Data_train, Data_test, imageShape)
-    ml = OneVsRestClassifier(MLPClassifier(max_iter = 1000)).fit(Data_train, Target_train)
-    return (ml.predict_proba(Data_test), ml.predict(Data_test))
+    ml = OneVsRestClassifier(MLPClassifier(max_iter = 2000)).fit(Data_train, Target_train)
+    return {"predict_proba": ml.predict_proba(Data_test),"predict": ml.predict(Data_test)}
 
 def LBP_SVM(Data_train, Data_test, Target_train, Target_test, imageShape):
     Data_train, Data_test = LBP(Data_train, Data_test, imageShape)
-    svc = OneVsRestClassifier(SVC()).fit(Data_train, Target_train)
-    return (svc.predict_proba(Data_test), svc.predict(Data_test))
+    svc = OneVsRestClassifier(SVC(probability=True, gamma = "auto")).fit(Data_train, Target_train)
+    return {"predict_proba": svc.predict_proba(Data_test),"predict": svc.predict(Data_test)}
 
 def LDA(Data_train, Data_test, Target_train, Target_test):
     lda = OneVsRestClassifier(LinearDiscriminantAnalysis()).fit(Data_train, Target_train)
-    return (lda.predict_proba(Data_test), lda.predict(Data_test))
+    return {"predict_proba": lda.predict_proba(Data_test),"predict": lda.predict(Data_test)}
 
 # --- Metricas
 def EER(fpr, tpr, threshold):
@@ -129,60 +129,97 @@ def ROC(classifier, Target_test, predict_Proba):
     
     return (fpr, tpr, threshold, roc_auc)
 
-def CMC(): #https://pythonhosted.org/bob/temp/bob.measure/doc/py_api.html
-    return 0
+def ConfusionMatrix(Target_test, predict): #https://pythonhosted.org/bob/temp/bob.measure/doc/py_api.html
+    return confusion_matrix(Target_test.dot(list(range(len(Target_test[0])))), predict.dot(list(range(len(Target_test[0])))), labels=range(len(Target_test[0])))
 
-def CurvasPrecisaoRevocacao():
-    return 0
+def CurvasPrecisaoRevocacao(classifier, Target_test, predict_Proba):
+    # For each class
+    precision = dict()
+    recall = dict()
+    average_precision = dict()
+    for i in range(len(Target_test[0])):
+        precision[i], recall[i], _ = precision_recall_curve(Target_test[:, i], predict_Proba[:, i])
+        average_precision[i] = average_precision_score(Target_test[:, i], predict_Proba[:, i])
+    
+    colors = cycle(['aqua', 'darkorange', 'cornflowerblue', 'red', 'green', 'blue'])
+
+    plt.figure(figsize=(7, 8))
+    f_scores = np.linspace(0.2, 0.8, num=4)
+    lines = []
+    labels = []
+    for f_score in f_scores:
+        x = np.linspace(0.01, 1)
+        y = f_score * x / (2 * x - f_score)
+        l, = plt.plot(x[y >= 0], y[y >= 0], color='gray', alpha=0.2)
+        plt.annotate('f1={0:0.1f}'.format(f_score), xy=(0.9, y[45] + 0.02))
+    
+    for i, color in zip(range(len(Target_test[0])), colors):
+        l, = plt.plot(recall[i], precision[i], color=color, lw=2)
+        lines.append(l)
+        labels.append('Precision-recall for class {0} (area = {1:0.2f})'
+                      ''.format(i, average_precision[i]))
+    
+    fig = plt.gcf()
+    fig.subplots_adjust(bottom=0.25)
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Extension of Precision-Recall curve to '+classifier)
+    plt.legend(lines, labels, loc=(0, -.38), prop=dict(size=14))
+    
+    
+    plt.show()
 
 def FMeasure(Target_test, Target_pred):
     return f1_score(Target_test.dot(list(range(len(Target_test[0])))), Target_pred.dot(list(range(len(Target_pred[0])))), average=None)  
 
-# --- Testes
-#def test1():
-#dataset
-Data_train, Data_test, Target_train, Target_test, imageShape = getDatas(min_faces_per_person = 120, test_size = 0.25, saveDataBase = True)
-
-# --- Extração de caracteristicas
-
-#LDA
-#LDA_PredictProba, LDA_Predict = LDA(Data_train, Data_test, Target_train, Target_test)
-
-#PCA + ML
-#ML_Score, LDA_Predict = LBP_ML(Data_train, Data_test, Target_train, Target_test, imageShape)
-
-# --- Metricas
-
-#ROC
-#fpr, tpr, threshold, roc_auc = ROC("LDA", Target_test, LDA_PredictProba) 
-#ROC("Machine learning", Target_test, ML_Score)
-
-#EER
-#eer, eer_threshold = EER(fpr, tpr, threshold)
-
-#F1_score = FMeasure(Target_test, LDA_Predict)
+# --- Teste
+def test(Data_train, Data_test, Target_train, Target_test, imageShape):   
+    # --- Extratores de caracteristicas e Classificadores
     
-#teste_1 = test1()
+    predicts = {"LDA": LDA(Data_train, Data_test, Target_train, Target_test), 
+                "PCA com Machine Learning": PCA_ML(Data_train, Data_test, Target_train, Target_test),
+                "LBP com Machine Learning": LBP_ML(Data_train, Data_test, Target_train, Target_test, imageShape), 
+                "LBP com Support Vector Machine": LBP_SVM(Data_train, Data_test, Target_train, Target_test, imageShape)}
 
-#ML_CM = PCA_ML(Data_train, Data_test, Target_train, Target_test)
-# 2 caracteriscas
-# todos metodos
-# tastar com pelo menos 2 de luz e obstrução de face
-# usar um tal de cmc para ver o q o dataset traz como mais proximo (cmc database recuperation)
+    # --- Metricas
+    roc_auc = dict()
+    eer = dict()
+    F1 = dict()
+    PRC = dict()
+    CM = dict()
+    for key in predicts.keys():
+        
+        print("Dados do classificador: "+key)
+        #
+        CM[key] = ConfusionMatrix(Target_test,predicts[key]["predict"])
+        
+        #ROC
+        fpr, tpr, threshold, roc_auc[key] = ROC(key, Target_test, predicts[key]["predict_proba"]) 
+        
+        #EER
+        eer[key], eer_threshold = EER(fpr, tpr, threshold)
+        
+        #CurvasPrecisaoRevocacao
+        PRC[key] = CurvasPrecisaoRevocacao(key, Target_test, predicts[key]["predict_proba"])
+        
+        #FMeasure
+        F1[key] = FMeasure(Target_test, predicts[key]["predict"])
+    
+    return (predicts, CM, roc_auc, eer, PRC, F1)
+    
+#Data_train, Data_test, Target_train, Target_test, imageShape = getDatas(min_faces_per_person = 120, test_size = 0.25, saveDataBase = True)
 
+predicts, CM, roc_auc, eer, PRC, F1 = test(Data_train, Data_test, Target_train, Target_test, imageShape)
 '''
-def LBP():
-    return 0
-    
-def PCA_SVC(Data_train, Data_test, Target_train, Target_test):
-    Data_train, Data_test = PCA(Data_train, Data_test)
-    svc = SVC()
-    svc = svc.fit(Data_train, Target_train)
-    return confusion_matrix(Target_test, svc.predict(Data_test))
+for i in range(len(Data_test)):
+    Data_test[i].reshape(imageShape)[int((imageShape[0]-10)/2):int((imageShape[0]+10)/2)] = 0
 
-def PCA_KNeighbors(Data_train, Data_test, Target_train, Target_test):
-    Data_train, Data_test = PCA(Data_train, Data_test)
-    kNeighbors = KNeighborsClassifier()
-    kNeighbors.fit(Data_train, Target_train)
-    return confusion_matrix(Target_test, kNeighbors.predict(Data_test))
+Teste2 = test(Data_train, Data_test, Target_train, Target_test, imageShape)
+
+for i in range(len(Data_test)):
+    Data_test[i].reshape(imageShape)[int((imageShape[0]-10)/2):int((imageShape[0]+10)/2)] = 255
+
+Teste3 = test(Data_train, Data_test, Target_train, Target_test, imageShape)
 '''
